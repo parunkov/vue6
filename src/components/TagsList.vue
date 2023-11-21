c8a57cf84c08107b72c00b2a8c69b449c503c9a2
parunkov <parunkov@gmail.com>
2023-11-21 14:34:45 +0500

    install fix

--- src/components/TagsList.vue
+++ src/components/TagsList.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <template v-for="(tag, index) in tags">
-            <div v-if="index > 0" :key="tag.text">-</div>
+            <div v-if="index > 0" :key="tag.text">
+                <v-icon>mdi-circle-small</v-icon>
+            </div>
             <div :key="tag.text">{{ tag.text }}</div>
         </template>
     </div>
