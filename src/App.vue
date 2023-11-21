fd39adc48ab4361652d538feddfbd8b6ebc71543
parunkov <parunkov@gmail.com>
2023-11-21 11:41:28 +0500

    vuetifi add

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <div id="app">
-    <TagsList :tags="tags" />
-  </div>
+  <v-app>
+    <v-main>
+      <TagsList :tags="tags" />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -9,27 +11,17 @@ import TagsList from './components/TagsList.vue';
 
 export default {
   name: 'App',
-  data() {
-    return {
+
+  components: {
+    TagsList
+  },
+
+  data: () => ({
       tags: [
         {text: '4,3'},
         {text: 'Фридланские ворота'},
         {text: '1,2 км от вас'}
       ]
-    }
-  },
-  components: {
-    TagsList
-  }
-}
+  }),
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
