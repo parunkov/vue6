a22f5df2afa61efac6e5fdff5f2bc4c92cb37b37
parunkov <parunkov@gmail.com>
2023-11-21 11:21:41 +0500

    tags list

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <TagsList :tags="tags" />
+  </div>
+</template>
+
+<script>
+import TagsList from './components/TagsList.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      tags: [
+        {text: '4,3'},
+        {text: 'Фридланские ворота'},
+        {text: '1,2 км от вас'}
+      ]
+    }
+  },
+  components: {
+    TagsList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TagsList.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <template v-for="(tag, index) in tags">
+            <div v-if="index > 0" :key="tag.text">-</div>
+            <div :key="tag.text">{{ tag.text }}</div>
+        </template>
+    </div>
+</template>
+<script>
+export default {
+    props: ['tags']
+}
+</script>
